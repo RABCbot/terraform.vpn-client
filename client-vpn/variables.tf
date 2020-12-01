2f4b8dc716e695aca80e7667224ec8bc8fae9460
Ricardo Balladares <RABCbot@users.noreply.github.com>
2020-12-01 08:56:31 -0500

    Add files via upload

---
+++ client-vpn/variables.tf
@@ -0,0 +1,8 @@
+variable "namespace" {}
+variable "stage" {}
+variable "name" {}
+variable "vpn_cidr" {}
+variable "server_certificate" {}
+variable "client_certificate" {}
+variable "subnet" {}
+variable "security_group" {}
